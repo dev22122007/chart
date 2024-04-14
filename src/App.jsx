b07d2ca5976eeb03dcc07c6bbd5394f7944b5234
dev22122007 <turdiboyevc@gmail.com>
2024-04-14 21:39:09 +0500

    bajarildi

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import CurrencyChart from './components/CurrensyChart';
+
+function App() {
+  return (
+    <div className="App">
+        <CurrencyChart></CurrencyChart>
+    </div>
+  );
+}
+
+export default App
